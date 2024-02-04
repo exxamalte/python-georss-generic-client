0ed79af2789e642fe63ca9cb2884515c9953a2e1
Malte Franken <exxamalte@users.noreply.github.com>
2024-02-04 23:22:32 +1100

    split code

---
+++ georss_generic_client/feed_entry.py
@@ -0,0 +1,16 @@
+"""Generic GeoRSS feed entry."""
+from georss_client import FeedEntry
+
+
+class GenericFeedEntry(FeedEntry):
+    """Generic feed entry."""
+
+    def __init__(self, home_coordinates, rss_entry, attribution):
+        """Initialise this service."""
+        super().__init__(home_coordinates, rss_entry)
+        self._attribution = attribution
+
+    @property
+    def attribution(self) -> str:
+        """Return the attribution of this entry."""
+        return self._attribution
---
+++ georss_generic_client/feed_manager.py
@@ -0,0 +1,27 @@
+"""Generic GeoRSS feed manager."""
+from georss_client.feed_manager import FeedManagerBase
+
+from .feed import GenericFeed
+
+
+class GenericFeedManager(FeedManagerBase):
+    """Feed Manager for Generic GeoRSS feed."""
+
+    def __init__(
+        self,
+        generate_callback,
+        update_callback,
+        remove_callback,
+        coordinates,
+        url,
+        filter_radius=None,
+        filter_categories=None,
+    ):
+        """Initialize the Generic GeoRSS Feed Manager."""
+        feed = GenericFeed(
+            coordinates,
+            url,
+            filter_radius=filter_radius,
+            filter_categories=filter_categories,
+        )
+        super().__init__(feed, generate_callback, update_callback, remove_callback)
